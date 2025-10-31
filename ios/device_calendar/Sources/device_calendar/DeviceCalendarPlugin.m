6dce9485e93e28b3634506a374a0c043089243c1
chikako-miyamae <chikako-miyamae-a@c.nml-nkm.com>
2025-10-31 19:26:15 +0900

    Remove unnecessary imports

--- ios/device_calendar/Sources/device_calendar/DeviceCalendarPlugin.m
+++ ios/device_calendar/Sources/device_calendar/DeviceCalendarPlugin.m
@@ -1,5 +1,4 @@
 #import "DeviceCalendarPlugin.h"
-#import <device_calendar/device_calendar-Swift.h>
 
 @implementation DeviceCalendarPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
